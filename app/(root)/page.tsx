909df7b92ad95b80c2bb8b7261cd6c1f47ca2ec5
Raghuram Megharaj <raghuram.megharaj@gmail.com>
2025-01-23 12:49:08 -0800

    Just before adding Zod validations

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
-const Homepage = () => {
-  return <>Prostore</>;
+import { getLatestProducts } from "@/lib/actions/product.actions";
+import ProductList from "@/components/shared/product/product-list";
+
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+  return (
+    <>
+      <ProductList
+        data={latestProducts}
+        title="Newest Arrivals"
+        limit={4}
+      ></ProductList>
+    </>
+  );
 };
 
 export default Homepage;
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -2,3 +2,5 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
 export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || 'A modern Indian Jewelery portal'
 
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4
